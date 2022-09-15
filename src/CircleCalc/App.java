c6d225c9dfdf73f152e87e2a2ce3ce0235d2a6b1
JubinJ0110 <jubinjos0110@gmail.com>
2022-09-15 18:50:57 -0500

    First commit and push to hw repo

---
+++ src/CircleCalc/App.java
@@ -0,0 +1,11 @@
+package CircleCalc;
+import java.util.Random;
+
+public class App {
+   public static void man(String[] args){
+        Random randomNum = new Random();
+        int randInt = randomNum.nextInt();
+
+        System.out.println(randInt);
+   } 
+}
