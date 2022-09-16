98e4b48415ffb96182ce8372ac8564a0f000f3fa
JubinJ0110 <jubinjos0110@gmail.com>
2022-09-16 00:53:26 -0500

    Commiting and Pushing CircleCalc Assignment to Git

--- src/CircleCalc/App.java
+++ src/CircleCalc/App.java
@@ -2,10 +2,17 @@ package CircleCalc;
 import java.util.Random;
 
 public class App {
-   public static void man(String[] args){
+   public static void main(String[] args){
         Random randomNum = new Random();
         int randInt = randomNum.nextInt();
+        randInt = Math.abs(randInt);
+        double area = Math.pow(randInt,2)*Math.PI;
+        double circum = Math.PI*2*randInt;
 
-        System.out.println(randInt);
+        System.out.printf("Radius of the Circle: %10d", randInt);
+        System.out.println();
+        System.out.printf("Area of the Circle: %10.2f", area);
+        System.out.println();
+        System.out.printf("Circumference of the Circle: %10.2f", circum);
    } 
 }
