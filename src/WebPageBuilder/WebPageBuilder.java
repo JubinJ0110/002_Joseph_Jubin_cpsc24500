44ac0ac6d3d99f6ba37275cda7312e0858fd946f
JubinJ0110 <jubinjos0110@gmail.com>
2022-09-20 17:16:21 -0500

    Added all questions and neccesary functions

---
+++ src/WebPageBuilder/WebPageBuilder.java
@@ -0,0 +1,27 @@
+package WebPageBuilder;
+import java.util.Scanner;
+
+public class WebPageBuilder {
+
+    public int computeAgeInMonths(int age){
+        return age*12;
+    }
+
+    public double computeQuotient(double x, double y){
+        return x/y;
+    }
+
+    public static void main(String[] args){
+        Scanner  bill = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        bill.next();
+        System.out.println("How old are you?");
+        int age = bill.nextInt();
+        System.out.println("Where do you live?");
+        String home = bill.next();
+        System.out.println("When you grow, you want to be...?");
+        String job = bill.next(); 
+        System.out.println("What are your 2 favorite integers?(Enter them separated with a space)");
+    }
+}
