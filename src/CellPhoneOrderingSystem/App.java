bc4c05888df874680dede134896c5fd9c94b771c
JubinJ0110 <jubinjos0110@gmail.com>
2022-10-10 15:22:28 -0500

    Added comments

--- src/CellPhoneOrderingSystem/App.java
+++ src/CellPhoneOrderingSystem/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class App {
 
+// Intro Function
     public static String intro(){
         String intro = """
 **************************************************
@@ -17,6 +18,7 @@ with various addons for your employees.
         return intro;
     }
     public static void main(String[] args) {
+// Creating and Initializing all variables for this assignment 
         Scanner cellScanner = new Scanner(System.in);
         int phoneNum;
         double total = 0;
@@ -27,11 +29,14 @@ with various addons for your employees.
         int storage;
         String choice = "";
 
-         System.out.println(intro());
+// Start user interaction
+        System.out.println(intro());
 
+// Get number of phones to buy
         System.out.print("How many phones will you purchase? ");
         phoneNum = cellScanner.nextInt();
-        
+
+// For loop for looping through phone specs and add-ons
         for(int i = 1;i<=phoneNum;i++){
             System.out.println("\nLet's configure phone #" + i + " ...");
             System.out.print("Enter A for Android or I for IPhone: ");
@@ -40,7 +45,8 @@ with various addons for your employees.
             if (i == 1){
                 brand = cellScanner.nextLine();
             }
-            
+
+// Brand of Phone
             if (brand.equals("A")){
                 total += 799;
                 bigString += "Android ";
@@ -49,6 +55,7 @@ with various addons for your employees.
                 bigString += "IPhone ";
             }
 
+// Screen size
             System.out.print("What size screen? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
             size = cellScanner.nextInt();
             if (size==1){
@@ -62,6 +69,7 @@ with various addons for your employees.
                 bigString += "6.7\" ";
             }
 
+// Storage
             System.out.print("How much storage? Enter 64, 128, or 256: ");
             storage = cellScanner.nextInt();
             choice = cellScanner.nextLine();
@@ -76,6 +84,7 @@ with various addons for your employees.
                 bigString += "256GB ";
             }
 
+// Loop through what add-ons the user wants
             while (!(choice.equals("F"))){
                 System.out.println("""
         What add-ons do you want?
@@ -100,12 +109,15 @@ with various addons for your employees.
                     bigString += "/ wireless charger ";
                 }
             }
+
+// Add to final Strings and add to total cost
             bigString += " ($" + total + ")\n";
             actualTotal += total;
             total = 0;
                 
         }
 
+// Summary of the user's purchase
         System.out.println("\nHere is the summary of your order:\n" + bigString);
         System.out.printf("Your total cost is: $%.2f\n\nThank you for your order.", actualTotal);
 
